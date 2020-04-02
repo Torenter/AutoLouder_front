cc4d4dbab932175928c3d7ee8f84ba80d099472f
Egor <59046481+Torenter@users.noreply.github.com>
2020-04-02 13:32:57 +0300

    Модель загрузки данных

--- profile_user/admin.py
+++ profile_user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import CustomFile
 # Register your models here.
+@admin.register(CustomFile)
+class CustomFileAdmin(admin.ModelAdmin):
+    list_display=['file_user','status','created']
+    list_filter = ['status']
--- profile_user/models.py
+++ profile_user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class CustomFile(models.Model):
+    file_user = models.FileField(upload_to="",)
+    status = models.CharField(max_length=50,default='Upload')
+    #user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE) - строка приводит к ошибке, но она нужно,чтобы знать какой юзер загрузил файл
+    created = models.DateField(auto_now_add=True, db_index=True )
