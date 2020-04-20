0855086e2049b1217e3c6e9825a5ca50d0c6ed75
Egor <59046481+Torenter@users.noreply.github.com>
2020-04-20 11:18:14 +0300

    RabbitMQ
    при удачном сохранении файла на сервере  происходит отправка в очередь RabbitMQ сообщения с тем где лежит файл и как он нащывается

--- profile_user/views.py
+++ profile_user/views.py
@@ -6,32 +6,30 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import CustomFile
-from django.contrib.auth.models import User
+from django.conf import settings
+#from django.contrib.auth.models import User
+import pika
     
 class UploadFileCreateView(LoginRequiredMixin,CreateView):
     login_url = '/login/'
     model = CustomFile
-    fields = ['file_user']
+    fields = ['file_user'] #Поле выводимое на экран из модели
     template_name = 'simple_upload.html' #путь к шаблону для вывода страницы
     success_url = reverse_lazy ('dashboard') #куда перенаправлять в случае удачной загрузки файла
-    def form_valid(self, form):
-        form.instance.user = self.request.user
+    def form_valid(self, form ):
+        form.instance.user = self.request.user # автозаполнение поля пользователя
+        connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost')
+        )
+        channel = connection.channel()
+        channel.queue_declare(queue= 'hello')
+        file_name=self.request.FILES['file_user'].name # в request.FILES хранится всё о файле. Через ключ мы обращаемся к полю с файлом, где содержится вся информация. И берет атрибут имени,чтобы знать как файл называется
+        channel.basic_publish(exchange='', routing_key='hello', body='{}\\{}'.format(settings.MEDIA_ROOT,file_name )) #отправка сообщения с дирректорией и названием файла
+
+        channel.close()
         return super().form_valid(form)
+    
 
 @login_required
 def dashboard(request):
     return render(request,'account/dashboard.html',{'section': 'dashboard'}) #первый вариант страницы личного кабинета
-#второй вариант, более рабочий но всё так же непонятный
-'''
-from django.core.files.storage import FileSystemStorage
-@login_required
-def dashboard(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request, 'simple_upload.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
-    return render(request, 'simple_upload.html')'''
