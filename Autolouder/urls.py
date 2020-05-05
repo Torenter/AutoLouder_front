9bfec7d4675bdea4a86bf62936f43892581b03ac
Egor <59046481+Torenter@users.noreply.github.com>
2020-05-05 11:14:07 +0300

    Смена пароля
    Добавлена возможность сменить пароль.
    Базовая форма подменена кастомной

--- Autolouder/urls.py
+++ Autolouder/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('password_change/',include('users.urls')),
     path('admin/', admin.site.urls),
     path('login/',include('users.urls')),
     path('',include('profile_user.urls')),
+   
 ]
--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,8 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-   # path('',views.user_login ,name = 'login'),
     path('',auth_views.LoginView.as_view() ,name = 'login'),
     path('logout/',auth_views.LogoutView.as_view() ,name = 'logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'), 
 ]
